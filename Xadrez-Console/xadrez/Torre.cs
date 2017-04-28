541b6e1663061a6aad6de11da354913000429e41
michael santos maeshiro <michael.maeshiro@yahoo.com.br>
2017-04-28 16:33:54 -0300

    Teste iniciais com movimentos possiveis das pecas

--- Xadrez-Console/xadrez/Torre.cs
+++ Xadrez-Console/xadrez/Torre.cs
@@ -1,4 +1,5 @@
-﻿using tabuleiro;
+﻿using System;
+using tabuleiro;
 
 namespace xadrez
 {
@@ -12,6 +13,85 @@ namespace xadrez
         public override string ToString()
         {
             return "T";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                while(tab.posicaoValida(pos) && podeMover(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                    if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    {
+                        break;
+                    }
+                    pos.linha = pos.linha - 1;
+                }
+            }
+
+            //abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                while (tab.posicaoValida(pos) && podeMover(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                    if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    {
+                        break;
+                    }
+                    pos.linha = pos.linha - 1;
+                }
+            }
+
+            //a direita
+            pos.definirValores(posicao.linha, posicao.coluna +1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                while (tab.posicaoValida(pos) && podeMover(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                    if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    {
+                        break;
+                    }
+                    pos.coluna = pos.coluna + 1;
+                }
+            }
+
+            //a esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                while (tab.posicaoValida(pos) && podeMover(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                    if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    {
+                        break;
+                    }
+                    pos.coluna = pos.coluna - 1;
+                }
+            }
+
+            return mat;
         }
     }
 }
